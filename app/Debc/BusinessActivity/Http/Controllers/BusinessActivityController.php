b87e8aeda2de91bb11029ad5b632debe4830234c
Thavreak <thavreak.log@gmail.com>
2022-10-14 11:35:52 +0700

    update migrate

---
+++ app/Debc/BusinessActivity/Http/Controllers/BusinessActivityController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Debc\BusinessActivity\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BusinessActivityController
+{
+    //
+}
---
+++ database/migrations/2022_10_14_041227_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('code');
+            $table->string('name_km');
+            $table->string('slug')->comment('ministry short name');
+            $table->string('m_name_km')->nullable()->comment('ministry full name');
+            $table->integer('parent_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
