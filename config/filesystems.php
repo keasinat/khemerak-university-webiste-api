18525c3973df08a4201418a90e0e5cdb0bb8240a
Thavreak <thavreak.log@gmail.com>
2023-01-24 09:06:32 +0700

    update config

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            // 'root' => storage_path('app/public'),
+            'root' => public_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -58,7 +59,7 @@ return [
 
         'uploads' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
