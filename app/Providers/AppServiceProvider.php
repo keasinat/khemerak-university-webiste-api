034047a287369eca1e4d70c2ea2e7b4035c3bffa
Thavreak <thavreak.log@gmail.com>
2023-03-28 11:34:37 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(env('APP_ENV') == 'production'){
+            $this->app['request']->server->set('HTTPS','on');
             URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(125);
     }
 }
