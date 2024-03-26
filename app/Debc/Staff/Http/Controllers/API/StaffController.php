f0563d5ef0e1000740e4ae1777a55fca10e28d0e
longthavreak <thavreak.long@gmail.com>
2024-03-26 17:16:47 +0700

    update staff

--- app/Debc/Staff/Http/Controllers/API/StaffController.php
+++ app/Debc/Staff/Http/Controllers/API/StaffController.php
@@ -4,14 +4,17 @@ namespace App\Debc\Staff\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Debc\Staff\Models\Staff;
+use App\Enum\isPublishedEnum;
+use App\Debc\Staff\Http\Resources\StaffResource;
 
 class StaffController extends Controller
 {
-
-
-    public function index()
+    public function index(Request $request)
     {
+        $paginate = $request->per_page ?? 12;
+        $staffs = Staff::where('is_published', isPublishedEnum::IS_PUBLUSHED)->paginate($paginate);
 
+        return StaffResource::collection($staffs);
     }
 }
